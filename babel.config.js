478a597a77e731fa681a8cfc9d8fcb24bec945fd
Aaron Victor <aeinsteinvikta@gmail.com>
2020-03-18 20:04:54 +0100

    example is complete

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,15 @@
 module.exports = {
   presets: [
-    '@vue/cli-plugin-babel/preset'
+    '@vue/cli-plugin-babel/preset',
+    [
+      "env",
+      {
+        "modules":false
+      }
+    ],
+    "stage-3"
+  ],
+  plugins:[
+    "syntax-dynamic-import"
   ]
 }
--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,12 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 
-import Hello from './components/Hello.vue'
-import World from './components/World.vue'
+// import Hello from './components/Hello.vue'
 
+// import World from './components/World.vue'
+
+const Hello= ()=>import('./components/Hello')
+const World =()=>import('./components/World')
 Vue.use(VueRouter);
 
 
@@ -13,6 +16,10 @@ const routes= [
     path:'/hello',
     name:'hello',
     component:Hello
+  },
+  {
+    path:'/',
+    redirect:'/hello'
   },
   {
     path:'/world',
